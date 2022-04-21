e06f9bcf23c6f633ccbe3a1abf80e3328494add6
ariel.plasencia <ariel.plasencia@gmail.com>
2022-04-21 00:13:27 -0400

    add console of buscaminas

---
+++ src/Buscaminas en Consola/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buscaminas_Logic;
+
+namespace Buscaminas_en_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game busc = new Game("Facil");
+            busc.Imprimir_Board_Bool();
+            string[] posicion = new string[2];
+
+            while (true)
+            {
+                int TipoJugada = 1;
+                Console.WriteLine("Elija una opcion:");
+                Console.WriteLine("1.Jugar Click Izquierdo");
+                Console.WriteLine("2.Jugar Click Derecho");
+                Console.WriteLine("3.Jugar Ambos Clicks");
+                switch (Console.ReadLine())
+                { 
+                    case "1":
+                        TipoJugada = 1;
+                        break;
+                    case "2":
+                        TipoJugada = 2;
+                        break;
+                    case "3":
+                        TipoJugada = 3;
+                        break;
+                }
+                posicion = Console.ReadLine().Split();
+                int fila = int.Parse(posicion[0]);
+                int col = int.Parse(posicion[1]);
+                switch (TipoJugada)
+                { 
+                    case 1:
+                        busc.Jugar(fila-1, col-1);
+                        break;
+                    case 2:
+                        busc.Clic_Der(fila-1, col-1);
+                        break;
+                    case 3:
+                        break;
+                }
+                
+                Console.Clear();
+                busc.Imprimir_Board_Bool();   
+            }
+        }
+    }
+}
---
+++ src/Buscaminas en Consola/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Buscaminas en Consola")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Buscaminas en Consola")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f774085-9fbc-4f64-8129-30463e2b6e96")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
