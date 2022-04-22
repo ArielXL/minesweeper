ec1d39e10d2824fedb0c754c1d7e5b0a746cd615
ariel.plasencia <ariel.plasencia@gmail.com>
2022-04-22 19:07:09 -0400

    add visual of buscaminas

---
+++ src/Buscaminas-Visual/Visual.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Buscaminas_Visual
+{
+    partial class Board_Visual
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Board_Visual));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labeltime = new System.Windows.Forms.Label();
+            this.labelCont = new System.Windows.Forms.Label();
+            this.labelminas = new System.Windows.Forms.Label();
+            this.pbxTablero = new System.Windows.Forms.PictureBox();
+            this.nuevoJuegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoJuegoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.reiniciarJuegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dificilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTablero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoJuegoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(518, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labeltime
+            // 
+            this.labeltime.AutoSize = true;
+            this.labeltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltime.Location = new System.Drawing.Point(450, 8);
+            this.labeltime.Name = "labeltime";
+            this.labeltime.Size = new System.Drawing.Size(63, 16);
+            this.labeltime.TabIndex = 3;
+            this.labeltime.Text = "Contador";
+            // 
+            // labelCont
+            // 
+            this.labelCont.AutoSize = true;
+            this.labelCont.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCont.Location = new System.Drawing.Point(385, 8);
+            this.labelCont.Name = "labelCont";
+            this.labelCont.Size = new System.Drawing.Size(67, 17);
+            this.labelCont.TabIndex = 4;
+            this.labelCont.Text = "Tiempo : ";
+            // 
+            // labelminas
+            // 
+            this.labelminas.AutoSize = true;
+            this.labelminas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelminas.Location = new System.Drawing.Point(193, 7);
+            this.labelminas.Name = "labelminas";
+            this.labelminas.Size = new System.Drawing.Size(45, 17);
+            this.labelminas.TabIndex = 5;
+            this.labelminas.Text = "Minas";
+            // 
+            // pbxTablero
+            // 
+            this.pbxTablero.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pbxTablero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxTablero.Image = ((System.Drawing.Image)(resources.GetObject("pbxTablero.Image")));
+            this.pbxTablero.Location = new System.Drawing.Point(1, 28);
+            this.pbxTablero.Name = "pbxTablero";
+            this.pbxTablero.Size = new System.Drawing.Size(512, 485);
+            this.pbxTablero.TabIndex = 1;
+            this.pbxTablero.TabStop = false;
+            this.pbxTablero.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxTablero_Paint_1);
+            this.pbxTablero.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbxTablero_MouseClick);
+            // 
+            // nuevoJuegoToolStripMenuItem
+            // 
+            this.nuevoJuegoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoJuegoToolStripMenuItem1,
+            this.reiniciarJuegoToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.opcionesToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.salirToolStripMenuItem});
+            this.nuevoJuegoToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.home;
+            this.nuevoJuegoToolStripMenuItem.Name = "nuevoJuegoToolStripMenuItem";
+            this.nuevoJuegoToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.nuevoJuegoToolStripMenuItem.Text = "Juego";
+            // 
+            // nuevoJuegoToolStripMenuItem1
+            // 
+            this.nuevoJuegoToolStripMenuItem1.Image = global::Buscaminas_Visual.Properties.Resources.arrow_curve;
+            this.nuevoJuegoToolStripMenuItem1.Name = "nuevoJuegoToolStripMenuItem1";
+            this.nuevoJuegoToolStripMenuItem1.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.nuevoJuegoToolStripMenuItem1.Size = new System.Drawing.Size(171, 22);
+            this.nuevoJuegoToolStripMenuItem1.Text = "Nuevo juego";
+            this.nuevoJuegoToolStripMenuItem1.Click += new System.EventHandler(this.nuevoJuegoToolStripMenuItem1_Click);
+            // 
+            // reiniciarJuegoToolStripMenuItem
+            // 
+            this.reiniciarJuegoToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.arrow_circle_315;
+            this.reiniciarJuegoToolStripMenuItem.Name = "reiniciarJuegoToolStripMenuItem";
+            this.reiniciarJuegoToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.reiniciarJuegoToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.reiniciarJuegoToolStripMenuItem.Text = "Reiniciar juego";
+            this.reiniciarJuegoToolStripMenuItem.Click += new System.EventHandler(this.reiniciarJuegoToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(168, 6);
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facilToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.dificilToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.icon_diagram;
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // facilToolStripMenuItem
+            // 
+            this.facilToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.control;
+            this.facilToolStripMenuItem.Name = "facilToolStripMenuItem";
+            this.facilToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F9;
+            this.facilToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.facilToolStripMenuItem.Text = "Fácil";
+            this.facilToolStripMenuItem.Click += new System.EventHandler(this.facilToolStripMenuItem_Click);
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.controlx2;
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F10;
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.normalToolStripMenuItem.Text = "Normal";
+            this.normalToolStripMenuItem.Click += new System.EventHandler(this.normalToolStripMenuItem_Click);
+            // 
+            // dificilToolStripMenuItem
+            // 
+            this.dificilToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.control_skip;
+            this.dificilToolStripMenuItem.Name = "dificilToolStripMenuItem";
+            this.dificilToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F11;
+            this.dificilToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.dificilToolStripMenuItem.Text = "Difícil";
+            this.dificilToolStripMenuItem.Click += new System.EventHandler(this.dificilToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(168, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Image = global::Buscaminas_Visual.Properties.Resources.cross_circle;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click_1);
+            // 
+            // Board_Visual
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(518, 518);
+            this.Controls.Add(this.labelminas);
+            this.Controls.Add(this.labelCont);
+            this.Controls.Add(this.labeltime);
+            this.Controls.Add(this.pbxTablero);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Board_Visual";
+            this.Text = "Buscaminas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTablero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxTablero;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoJuegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoJuegoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem reiniciarJuegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dificilToolStripMenuItem;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labeltime;
+        private System.Windows.Forms.Label labelCont;
+        private System.Windows.Forms.Label labelminas;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ src/Buscaminas-Visual/Visual.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Buscaminas_Logic;
+using Buscaminas_en_Consola;
+using System.Diagnostics;
+
+namespace Buscaminas_Visual
+{
+    public partial class Board_Visual : Form
+    {
+        private Game juego;
+        private int dimen_fila;
+        private int dimen_col;
+        private string dificultad = "Facil";
+        Stopwatch crono;
+       
+        public Board_Visual()
+        {
+            InitializeComponent();
+            juego = new Game(dificultad);
+            dimen_fila = juego.Board.GetLength(0);
+            dimen_col = juego.Board.GetLength(1);
+            crono = new Stopwatch();
+            dificultad = juego.Dificultad;
+            labeltime.Text = "0";
+            labelminas.Text = "Minas: " + juego.Minas;
+        }
+
+        private void pbxTablero_Paint_1(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Bitmap casilla = new Bitmap(@"Figuritas\celda_vacia.bmp",true);
+            System.Drawing.Bitmap pregunta = new Bitmap(@"Figuritas\signo.bmp", true);
+            System.Drawing.Bitmap bandera = new Bitmap(@"Figuritas\fig_bandera.bmp", true);
+            System.Drawing.Bitmap mina = new Bitmap(@"Figuritas\bombabmp.bmp",true);
+            int[,] matriz = juego.Board;
+            Game.Propiedad_Celda[,] marc = juego.Board_Juego;
+            Graphics g = e.Graphics;
+            SolidBrush b = new SolidBrush(Color.White);
+            g.FillRectangle(b, e.ClipRectangle);
+            Pen p = new Pen(Color.Black);
+            int width = pbxTablero.Width / dimen_col;
+            int altura = pbxTablero.Height / dimen_fila;
+            pbxTablero.Width = width * dimen_col;
+            pbxTablero.Height = altura * dimen_fila;
+            for (int i = 0; i < dimen_fila; i++)
+                for (int j = 0; j < dimen_col; j++)
+                {
+                    PointF punto = new PointF(j * width, i * altura);
+                    var tamaño = new SizeF(width, altura);
+                    var rectangulo = new RectangleF(punto, tamaño);
+                    switch (juego.Board_Juego[i, j])
+                    {
+                        case Game.Propiedad_Celda.Tapado:
+                           
+                            g.DrawImage(casilla, rectangulo);
+                            break;
+                        case Game.Propiedad_Celda.Descubierto:
+                           
+                            if (juego.Board[i,j]==0)
+                            g.FillRectangle(b, rectangulo);
+                            else if(juego.Board[i,j] == -1)                         
+                            {
+                                g.DrawImage(mina, rectangulo);
+                            }
+                            else
+                            {
+                                var format = new StringFormat();
+                                format.Alignment=StringAlignment.Center;
+                                format.LineAlignment = StringAlignment.Center;
+                                var bn = new SolidBrush(Color.Black);
+                                var font  = new Font (Font.FontFamily, Math.Min(width/2,altura/2));
+                                g.DrawString(juego.Board[i,j].ToString(),font,bn,rectangulo,format);
+                            }
+                            break;
+                        case Game.Propiedad_Celda.Bandera:
+                            g.DrawImage(bandera, rectangulo);
+                            break;
+                        case Game.Propiedad_Celda.Pregunta:
+                            g.DrawImage(pregunta, rectangulo);
+                            break;
+                    }
+                }
+            for (int i = 0; i <= dimen_fila; i++)
+            {
+                g.DrawLine(p, 0, i * altura - 1, pbxTablero.Width, i * altura - 1);
+            }
+            for (int i = 0; i <= dimen_col; i++)
+            {
+                g.DrawLine(p, i * width - 1, 0, i * width - 1, pbxTablero.Height);
+            }
+        }
+
+        private void pbxTablero_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i = e.Y / (pbxTablero.Height / dimen_fila);
+            int j = e.X / (pbxTablero.Width / dimen_col);
+            crono.Start();
+
+            switch (e.Button)
+            { 
+                case MouseButtons.Left:
+                    if (juego.Board_Juego[i, j] != Game.Propiedad_Celda.Bandera)
+                    {
+                        juego.Clic_Izq(i, j);
+                        if (juego.Jugar(i, j) == Game.Estado_Juego.Ganado)
+                        {
+                            crono.Stop();
+                            pbxTablero.Refresh();
+                            if (MessageBox.Show("Juego terminado, haz ganado.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                            {
+                                pbxTablero.Enabled = false;
+                                pbxTablero.Invalidate();
+
+                            }
+                        }
+                        if (juego.Jugar(i, j) == Game.Estado_Juego.Perdido)
+                        {
+                            crono.Stop();
+                            pbxTablero.Refresh();
+                            if (MessageBox.Show("Juego terminado, haz perdido.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                            {
+                                pbxTablero.Invalidate();
+                                pbxTablero.Enabled = false;
+                                dimen_fila = juego.Board.GetLength(0);
+                                dimen_col = juego.Board.GetLength(1);
+                            }
+                        }
+                    }
+                    pbxTablero.Refresh();
+                    break;
+                case MouseButtons.Right:
+                    juego.Clic_Der(i, j);
+                    labelminas.Text = "Minas: " + juego.Minas;
+                    pbxTablero.Refresh();
+                    break;
+            }
+        }
+
+        private void nuevoJuegoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Quieres empezar un nuevo juego?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                juego = new Game(dificultad);
+                crono.Restart();
+                crono.Stop();
+                labelminas.Text = "Minas: " + juego.Minas;
+                pbxTablero.Invalidate();
+                pbxTablero.Enabled = true;
+                dimen_fila = juego.Board.GetLength(0);
+                dimen_col = juego.Board.GetLength(1);
+            }
+        }
+
+        private void salirToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Quieres salir del juego?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                Close();
+        }
+
+        private void reiniciarJuegoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Quieres reiniciar este juego?", "Confimación", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                juego.Reiniciar_Juego();
+                crono.Restart();
+                crono.Stop();
+                labelminas.Text = "Minas: " + juego.Minas;
+                pbxTablero.Enabled = true;
+                pbxTablero.Invalidate();
+            }
+        }
+
+        private void facilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            juego = new Game("Facil");
+            dificultad = "Facil";
+            dimen_fila = juego.Board.GetLength(0);
+            dimen_col = juego.Board.GetLength(1);
+            pbxTablero.Invalidate();
+            pbxTablero.Enabled = true;
+            pbxTablero.Height = 458;
+            pbxTablero.Width = 512;
+            labelminas.Location = new Point(231, 7);
+            labelCont.Location = new Point(387, 7);
+            labeltime.Location = new Point(462, 7);            
+            crono.Restart();
+            crono.Stop();
+        }
+
+        private void normalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            juego = new Game("Normal");
+            dificultad = "Normal";
+            dimen_fila = juego.Board.GetLength(0);
+            dimen_col = juego.Board.GetLength(1);
+            pbxTablero.Invalidate();
+            pbxTablero.Enabled = true;
+            pbxTablero.Height = 512;
+            pbxTablero.Width = 600;
+            labelminas.Location = new Point(275, 7);
+            labelCont.Location = new Point(475, 7);
+            labeltime.Location = new Point(550, 7);
+            crono.Restart();
+            crono.Stop();
+        }
+
+        private void dificilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            juego = new Game("Dificil");
+            dificultad = "Dificil";
+            dimen_fila = juego.Board.GetLength(0);
+            dimen_col = juego.Board.GetLength(1);
+            pbxTablero.Invalidate();
+            pbxTablero.Enabled = true;
+            pbxTablero.Height = 600;
+            pbxTablero.Width = 1250;
+            labelminas.Location = new Point(600, 7);
+            labelCont.Location = new Point(1125, 7);
+            labeltime.Location = new Point(1200, 7);
+            crono.Restart();
+            crono.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            long tiempo = crono.ElapsedMilliseconds / 1000;
+            labeltime.Text = tiempo.ToString();
+        }
+        
+    }
+}
